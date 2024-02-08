c4651d404dcf5df75d591337153309f10fa925a2
gustavo <gustavo.nbispo@outlook.com>
2024-02-08 07:50:55 -0300

    define number of correct questions

--- index.js
+++ index.js
@@ -93,6 +93,12 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
+const corretas = new Set()
+
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
 for(const item of perguntas) {
 
   const quizItem = template.content.cloneNode(true)
@@ -101,6 +107,16 @@ for(const item of perguntas) {
   for(let resposta of item.respostas) {
     const dt = quizItem.querySelector('dl dt').cloneNode(true)
     dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+    dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta
+      corretas.delete(item)
+      if(estaCorreta) {
+        corretas.add(item)
+      }
+      mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+    } 
     quizItem.querySelector('dl').appendChild(dt)
   }
 
